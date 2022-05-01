4f7006ddcdf5ce71f84c5021957eed8ea058eb77
mbuechmann <malte.buechmann@posteo.de>
2022-05-01 22:25:18 +0200

    Add basic cobra command

--- cmd/terminalblaster/main.go
+++ cmd/terminalblaster/main.go
@@ -4,40 +4,40 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/spf13/cobra"
+
 	lib "github.com/mbuechmann/terminalblaster/internal/library"
 	"github.com/mbuechmann/terminalblaster/internal/ui"
 )
 
+var cmd = &cobra.Command{
+	Use:   "terminalblaster",
+	Short: "Blast your music from the terminal",
+	Long: `A lightweight music player for your terminal.
+		Play all your music from the command line.`,
+	Args: cobra.ExactValidArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := ui.Init(); err != nil {
+			fmt.Println("Could not open ui")
+			os.Exit(1)
+		}
+
+		path := os.Args[1]
+		c, err := lib.Load(path)
+		if err != nil {
+			panic(err)
+		}
+
+		if err := ui.OpenLoadScreen(c); err != nil {
+			panic(err)
+		}
+
+		ui.OpenLibraryScreen()
+
+		ui.Close()
+	},
+}
+
 func main() {
-	checkArgs()
-	loadLibrary()
-}
-
-func checkArgs() {
-	args := os.Args
-	if len(args) != 2 {
-		fmt.Println("Provide directory to be loaded")
-		os.Exit(1)
-	}
-}
-
-func loadLibrary() {
-	if err := ui.Init(); err != nil {
-		fmt.Println("Could not open ui")
-		os.Exit(1)
-	}
-
-	path := os.Args[1]
-	c, err := lib.Load(path)
-	if err != nil {
-		panic(err)
-	}
-
-	if err := ui.OpenLoadScreen(c); err != nil {
-		panic(err)
-	}
-
-	ui.OpenLibraryScreen()
-
-	ui.Close()
+	cmd.Execute()
 }
